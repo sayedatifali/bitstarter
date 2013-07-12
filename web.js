3c5b9f10b21213ddddcc06428cd78a44d2a4071a
Sayed Atif ALi <sayed.atif.ali@gmail.com>
2013-07-12 07:45:07 +0000

    Modified web.js to read from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    response.send(data.toString('utf-8'));
+  });
 });
 
 var port = process.env.PORT || 5000;
